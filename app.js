57148c25cf9d20a7c07cc1ef8b0e4346305b4717
Nihat Yusuf <nihatyusufuslu@gmail.com>
2022-04-28 04:15:34 +0300

    The End

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 class Course {
   constructor(title, instructor, image) {
+    this.courseId = Math.floor(Math.random() * 10000);
     this.title = title;
     this.instructor = instructor;
     this.image = image;
@@ -15,7 +16,7 @@ class UI {
                 <td><img src="img/${course.image}"></td>
                 <td>${course.title}</td>
                 <td>${course.instructor}</td>
-                <td><a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>
+                <td><a href="#" data-id="${course.courseId}" class="btn btn-danger btn-sm delete ms-2">Delete</a></td>
             </tr>
         `;
     list.innerHTML += html;
@@ -30,11 +31,83 @@ class UI {
   deleteCourse(element) {
     if (element.classList.contains("delete")) {
       element.parentElement.parentElement.remove();
+      return true;
     }
+  }
+
+  deleteAll(element) {
+    if (element.classList.contains("deleteAll")) {
+      let elements = document.querySelector("#course-list");
+      while (elements.firstChild) {
+        elements.removeChild(elements.firstChild);
+      }
+    }
+  }
+
+  showAlert(message, className) {
+    let alert = `
+      <div class="alert alert-${className}">
+        ${message}
+      </div>
+    `;
+    const row = document.querySelector(".row");
+    row.insertAdjacentHTML("beforeBegin", alert);
+
+    setTimeout(() => {
+      document.querySelector(".alert").remove();
+    }, 3000);
   }
 }
 
-document.getElementById("save").addEventListener("click", function (e) {
+class Storage {
+  static getCourses() {
+    let courses;
+    if (localStorage.getItem("courses") === null) {
+      courses = [];
+    } else {
+      courses = JSON.parse(localStorage.getItem("courses"));
+    }
+    return courses;
+  }
+
+  static displayCourses() {
+    const courses = Storage.getCourses();
+
+    courses.forEach((course) => {
+      const ui = new UI();
+      ui.addCourseToList(course);
+    });
+  }
+
+  static addCourse(course) {
+    const courses = Storage.getCourses();
+    courses.push(course);
+    localStorage.setItem("courses", JSON.stringify(courses));
+  }
+
+  static deleteCourse(element) {
+    if (element.classList.contains("delete")) {
+      const id = element.getAttribute("data-id");
+
+      const courses = Storage.getCourses();
+
+      courses.forEach((course, index) => {
+        if (course.courseId == id) {
+          courses.splice(index, 1);
+        }
+      });
+      localStorage.setItem("courses", JSON.stringify(courses));
+    }
+  }
+
+  static deleteAllCourses() {
+    localStorage.clear();
+  }
+}
+
+document.addEventListener("DOMContentLoaded", Storage.displayCourses);
+
+document.getElementById("new-course").addEventListener("submit", function (e) {
   const title = document.getElementById("title").value;
   const instructor = document.getElementById("instructor").value;
   const image = document.getElementById("image").value;
@@ -43,14 +116,32 @@ document.getElementById("save").addEventListener("click", function (e) {
 
   const ui = new UI();
 
-  ui.addCourseToList(course);
+  if (title === "" || instructor === "" || image === "") {
+    ui.showAlert("Please complete the form", "warning");
+  } else {
+    ui.addCourseToList(course);
 
-  ui.clearControls();
+    Storage.addCourse(course);
+
+    ui.clearControls();
+
+    ui.showAlert("The course has been added", "success");
+  }
 
   e.preventDefault();
 });
 
 document.getElementById("course-list").addEventListener("click", function (e) {
   const ui = new UI();
-  ui.deleteCourse(e.target);
+  if (ui.deleteCourse(e.target) == true) {
+    Storage.deleteCourse(e.target);
+    ui.showAlert("The course has been deleted", "danger");
+  }
+});
+
+document.querySelector(".deleteAll").addEventListener("click", function (e) {
+  const ui = new UI();
+  ui.deleteAll(e.target);
+  Storage.deleteAllCourses();
+  ui.showAlert("All courses has been deleted", "danger");
 });
